80e0146cc34ce68312bd3e8a3927ea618083fe20
kdurril <kdurril@gmail.com>
2015-05-14 21:08:55 -0400

    test update

--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,33 @@
+#!/usr/bin/env python3
 
+# encoding: utf-8
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
 
 import unittest
 import tika
 import tika.parser
-import tika.tika
-#from english_class import AttrSet, row_synth, row_keys, to_csv, to_json
-
-
-#sys.path.append('..')
 
 class CreateTest(unittest.TestCase):
-    "Test for New Generator.py Methods"
+    "Test for local and remote file request"
 
+    def test_init_vm(self):
+        'initialize VM'
+        self.asssetTrue(tika.initVM())
+        
     def test_local_pdf(self):
         'Parse local PDF'
         self.assertTrue(tika.parser.from_file('201504160015.pdf'))
--- tika/tika.py
+++ tika/tika.py
@@ -14,14 +14,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
 
-#from __future__ import print_function
-#from __future__ import unicode_literals
+
+
 import sys, os, getopt, time
-#from warnings import warn
-#from future.standard_library import install_aliases
-#install_aliases()
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
